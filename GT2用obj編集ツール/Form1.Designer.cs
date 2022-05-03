5c62b4ff00ba888b48b118d497622063d4558aa3
うどん01 <104125960+udon01@users.noreply.github.com>
2022-05-03 21:47:30 +0900

    Add files via upload

---
+++ GT2用obj編集ツール/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GT2用obj編集ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File_open = new System.Windows.Forms.Button();
+            this.Convert = new System.Windows.Forms.Button();
+            this.OBJpath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // File_open
+            // 
+            this.File_open.Location = new System.Drawing.Point(12, 12);
+            this.File_open.Name = "File_open";
+            this.File_open.Size = new System.Drawing.Size(408, 24);
+            this.File_open.TabIndex = 0;
+            this.File_open.Text = "ファイルを開く";
+            this.File_open.UseVisualStyleBackColor = true;
+            this.File_open.Click += new System.EventHandler(this.File_open_Click);
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(165, 80);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(100, 24);
+            this.Convert.TabIndex = 4;
+            this.Convert.Text = "変換";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // OBJpath
+            // 
+            this.OBJpath.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::GT2用obj編集ツール.Properties.Settings.Default, "OBJpathsave", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.OBJpath.Location = new System.Drawing.Point(12, 47);
+            this.OBJpath.Name = "OBJpath";
+            this.OBJpath.ReadOnly = true;
+            this.OBJpath.Size = new System.Drawing.Size(408, 22);
+            this.OBJpath.TabIndex = 5;
+            this.OBJpath.Text = global::GT2用obj編集ツール.Properties.Settings.Default.OBJpathsave;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 118);
+            this.Controls.Add(this.OBJpath);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.File_open);
+            this.Name = "Form1";
+            this.Text = "GT2用obj編集ツール";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button File_open;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.TextBox OBJpath;
+    }
+}
+
---
+++ GT2用obj編集ツール/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GT2用obj編集ツール
+{
+    public partial class Form1 : Form
+    {
+        public string controlfile;
+        public string line;
+        public string line1;
+        public string line2;
+        public int i = 0;
+        public string linef;
+        public bool flags = false;
+        public bool otog = false;
+        public bool saveline = false;
+        public string controlfilesave;
+        public OpenFileDialog ofd = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void File_open_Click(object sender, EventArgs e)
+        {
+            //「ファイル名」で表示される文字列を指定する
+            ofd.FileName = ".obj";
+            //表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = "";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = ".obj(*.obj;)|*.obj;";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "開くobjファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+                OBJpath.Text = ofd.FileName;
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            StreamReader read = new StreamReader(OBJpath.Text);
+            controlfilesave = OBJpath.Text;
+            controlfilesave = controlfilesave + ".txt";
+            StreamWriter save = new StreamWriter(controlfilesave);
+            save.NewLine = "\n";
+            while (read.Peek() > -1)
+            {
+                i = i + 1;
+                flags = false;
+                otog = false;
+                saveline = false;
+                line = read.ReadLine();
+
+                if (line.Contains("flags") == true)
+                {
+                    line1 = line.Replace(".001", "").Replace(".002", "").Replace(".003", "").Replace(".004", "")
+                        .Replace(".005", "").Replace(".006", "").Replace(".007", "").Replace(".008", "")
+                        .Replace(".009", "").Replace(".010", "").Replace("_NONE", "");
+                    flags = true;
+                    goto label1;
+                }
+
+                if (line.Contains("o ") == true)
+                {
+                    line2 = line.Replace("o ", "g ");
+                    otog = true;
+                    goto label1;
+                }
+
+            label1:;
+                if (line.Contains("#") == false && line.Contains("s ") == false)
+                {
+                    if (flags == true)
+                    {
+                        save.WriteLine(line1);
+                        saveline = true;
+                    }
+                    if (otog == true)
+                    {
+                        save.WriteLine(line2);
+                        saveline = true;
+                    }
+                    if (saveline == false)
+                        save.WriteLine(line);
+                }
+                else if (line.Contains("#") == true)
+                {
+                    i = i - 1;
+                }
+                if (i == 3)
+                {
+                    linef = "f 1 1 1";
+                    save.WriteLine(linef);
+                }
+                if (i == 5)
+                {
+                    linef = "f 2 2 2";
+                    save.WriteLine(linef);
+                }
+                if (i == 7)
+                {
+                    linef = "f 3 3 3";
+                    save.WriteLine(linef);
+                }
+                if (i == 9)
+                {
+                    linef = "f 4 4 4";
+                    save.WriteLine(linef);
+                }
+            }
+
+            read.Close();
+            save.Close();
+            File.Delete(OBJpath.Text);
+            File.Copy(OBJpath.Text + ".txt", OBJpath.Text);
+            File.Delete(OBJpath.Text + ".txt");
+            i = 0;
+            MessageBox.Show("終了しました");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
