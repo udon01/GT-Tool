70127c41d124e0fb2313b2e429c805c498656138
うどん01 <104125960+udon01@users.noreply.github.com>
2022-08-30 07:40:04 +0900

    Add files via upload

---
+++ GT3用モデル抽出ツール/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace GT3用モデル抽出ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.BackgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(10, 9);
+            this.ProgressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(314, 18);
+            this.ProgressBar1.TabIndex = 0;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(10, 31);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(191, 15);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "exeに直接ドラッグアンドドロップしてね！";
+            // 
+            // BackgroundWorker1
+            // 
+            this.BackgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.BackgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            this.BackgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 52);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.ProgressBar1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "GT3用モデル抽出ツール v1.5";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ProgressBar ProgressBar1;
+        private Label Label1;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker1;
+    }
+}
